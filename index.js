098d884002bff421579b59d4815f8f9f61b7070e
Albert Vallverdu <Albert.Vallverdu@reachplc.com>
2020-12-30 21:54:54 +0000

    split bot logic between getChatId and sendPhoto

--- index.js
+++ index.js
@@ -13,36 +13,37 @@ debug('booting', name);
 debug('set telegram Token =>', TELEGRAM_TOKEN !== undefined ? '👍 TOKEN PROVIDED 👍' : ' 👎 NO TOKEN PROVIDED 👎');
 
 if (!TELEGRAM_TOKEN) {
-  process.exit(1)
+  process.exit(0)
 }
 
 const bot = new TelegramBot(TELEGRAM_TOKEN);
 const luckyNumbers = LUCKY_NUMBERS.split(',');
+let groupId;
+
+(async () => {
+  try {
+    const botUpdate = await getChatId(TELEGRAM_TOKEN)
+
+    if (!Array.isArray(botUpdate)) {
+      throw new Error('result from getChatId is not an Array')
+    }
+
+    // Check if the groupId could have changed
+    debug('Getting Chat Id')
+    groupId = botUpdate.length === 0 ?
+      TELEGRAM_GROUP_CHAT_ID :
+      botUpdate.find(items => items.chat.type === 'group').chat.id;
+
+  } catch (e) {
+    throw new Error(e)
+  }
+})()
 
 scrapper({ debug, headless: JSON.parse(HEADLESS_CHROME), luckyNumbers })
   .then(() => {
-    (async () => {
-      try {
-        const botUpdate = await getChatId(TELEGRAM_TOKEN)
-
-        if (!Array.isArray(botUpdate)) {
-          throw new Error('result from getChatId is not an Array')
-        }
-
-        // Check if the groupId could have changed
-        debug('Getting Chat Id')
-
-        const groupId = botUpdate.length === 0 ?
-          TELEGRAM_GROUP_CHAT_ID :
-          botUpdate.find(items => items.chat.type === 'group').chat.id;
-
-        debug('Sending Picture')
-        bot.sendPhoto(groupId, './lotto_results.png').then(resp => {
-          debug('Picture sent', resp)
-          process.exit(0)
-        })
-      } catch (e) {
-        throw new Error(e)
-      }
-    })()
+    debug('Sending Picture')
+    bot.sendPhoto(groupId, './lotto_results.png').then(resp => {
+      debug('Picture sent', resp)
+      process.exit(0)
+    })
   });
