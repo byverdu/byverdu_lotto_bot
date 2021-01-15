842588dda48aca1dd9e6ff9c8e332307de98e26a
Albert Vallverdu <byverdu@gmail.com>
2021-01-15 11:51:14 +0000

    updated cronjob script

--- cronjob.sh
+++ cronjob.sh
@@ -1,7 +1,15 @@
 #!/bin/zsh
 
+# set cronjobs crontab -e
+
+# Run telegram bot every wed - sat at 9am1
+# * 9 * * wed,sat (cd /Users/albertvallverdu/Projects/repos/byverdu_lotto_bot && ./cronjob.sh)
+
 # Add timestamp
-echo "$(date)" >> log.txt
+echo "start bot => $(date)" >> log.txt
 
 # Run Telegram bot
 yarn start
+
+# Add timestamp
+echo "end bot => $(date)" >> log.txt
