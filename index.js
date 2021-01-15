b545be570f4a2e501e49bb5df7c3bec51312d089
Albert Vallverdu <byverdu@gmail.com>
2021-01-15 11:56:37 +0000

    error handleing

--- index.js
+++ index.js
@@ -3,8 +3,7 @@ require('dotenv').config()
 const TelegramBot = require('node-telegram-bot-api');
 const debug = require('debug')('node-telegram-bot-api');
 
-const { getChatId } = require('./utils');
-const { scrapper } = require('./lib')
+const { scrapper, utils: { logErrorBuilder } } = require('./lib')
 
 const name = 'byverdu_lotto_bot';
 const { TELEGRAM_TOKEN, TELEGRAM_GROUP_CHAT_ID, LUCKY_NUMBERS, HEADLESS_CHROME } = process.env;
@@ -19,31 +18,47 @@ if (!TELEGRAM_TOKEN) {
 const bot = new TelegramBot(TELEGRAM_TOKEN);
 const luckyNumbers = LUCKY_NUMBERS.split(',');
 let groupId;
+let telegramError;
 
 (async () => {
   try {
-    const botUpdate = await getChatId(TELEGRAM_TOKEN)
-
-    if (!Array.isArray(botUpdate)) {
-      throw new Error('result from getChatId is not an Array')
-    }
+    const botUpdate = await bot.getUpdates()
 
     // Check if the groupId could have changed
     debug('Getting Chat Id')
     groupId = botUpdate.length === 0 ?
       TELEGRAM_GROUP_CHAT_ID :
-      botUpdate.find(items => items.chat.type === 'group').chat.id;
+      botUpdate.find(items => items.message.chat.type === 'group').message.chat.id;
 
   } catch (e) {
-    throw new Error(e)
+    logErrorBuilder(`Error fetching botGroupId\n ${e}`)
+      .then(() => {
+        telegramError = 'fetching botGroupId Error logged'
+        debug('fetching botGroupId Error logged')
+      })
+      .catch(err => debug(err))
+    throw new Error(telegramError)
   }
 })()
-
-scrapper({ debug, headless: JSON.parse(HEADLESS_CHROME), luckyNumbers })
   .then(() => {
-    debug('Sending Picture')
-    bot.sendPhoto(groupId, './lotto_results.png').then(resp => {
-      debug('Picture sent', resp)
-      process.exit(0)
-    })
-  });
+    scrapper({ debug, headless: JSON.parse(HEADLESS_CHROME), luckyNumbers })
+      .then(() => {
+        debug('Sending Picture')
+        bot.sendPhoto(groupId, './lotto_results.png')
+          .then(resp => {
+            debug('Picture sent', resp)
+            process.exit(0)
+          })
+      })
+      .catch(err => {
+        logErrorBuilder(`Error in scrapper\n ${err}`)
+          .then(() => {
+            bot.sendMessage(groupId, `${err}`)
+              .then(() => {
+                debug('Scrapper Error sent')
+                process.exit(0)
+              })
+          })
+          .catch(err => debug(err))
+      });
+  })
